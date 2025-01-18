d279855c93f0ff52170fe934d216912b2e359184
dinitha-lakshan <rathnayaka_rdl_e23@engug.ruh.ac.lk>
2025-01-18 23:13:23 +0530

    changed height to 80

--- frontend/src/app/dinitha/page.tsx
+++ frontend/src/app/dinitha/page.tsx
@@ -1,9 +1,9 @@
 const Page = ()=>{
     return(
         <div className="flex flex-col ">
-            <div className="bg-amber-600 w-2/3 h-40"></div>
+            <div className="bg-amber-600 w-2/3 h-80"></div>
             <div>Dinitha</div>
-            <div className="bg-amber-600 w-2/3 h-40"></div>
+            <div className="bg-amber-600 w-2/3 h-80"></div>
         </div>
     )
 }

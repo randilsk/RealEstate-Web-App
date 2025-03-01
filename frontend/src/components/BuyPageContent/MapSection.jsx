9de60818828f8af2be5ca255cf7e49136de26002
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-03-01 17:17:16 +0530

    add margin and fix the heights of the map

--- frontend/src/components/BuyPageContent/MapSection.jsx
+++ frontend/src/components/BuyPageContent/MapSection.jsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 function MapSection() {
   return (
     <>
-      <div className="pt-10">
+      <div className="pt-10 mt-10 mr-10">
         <div>
           <Image
-            className="w-[850px] h-[513px]"
+            className="w-[850px] h-[600px]"
             src="/images/buy-image/map-image.png"
             width={720}
             height={650}

9bd27b9a43e78716cecdc0a1119600cde0cf451b
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-03-28 20:38:15 +0530

    don some changes in header file

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,15 @@
+"use client";  // This directive makes it a Client Component
+
 import Link from "next/link";
 import React from "react";
+import { useSelector } from "react-redux";
+import { User } from "lucide-react";
 
-const NavItem = ({ children, isBold }) => (
-  <div className="p-2.5 flex justify-center items-center gap-2.5">
+const NavItem = ({ children, isBold, onClick = () => {} }) => (
+  <div 
+    className="p-2.5 flex justify-center items-center gap-2.5 cursor-pointer"
+    onClick={onClick}
+  >
     <div
       className={`text-white text-xl ${isBold ? "font-bold" : "font-normal"}`}
     >
@@ -12,17 +19,19 @@ const NavItem = ({ children, isBold }) => (
 );
 
 function Header() {
+  const currentUser = useSelector((state) => state.user.currentUser);
+
   return (
     <div
       className="h-navbar px-[30px] py-1 bg-[#3b50df] mt-5 rounded-[50px] flex justify-between items-center font-poppins mx-auto"
       style={{ maxWidth: "calc(100% - 200px)" }}
     >
-      <div className="flex justify-start items-center gap-7}">
-        <NavItem className="hover:cursor-pointer">
+      <div className="flex justify-start items-center gap-7">
+        <NavItem>
           <Link href={"/buy"}>Buy</Link>
         </NavItem>
         <NavItem>
-          <Link href={"/sell"}>Sell</Link>{" "}
+          <Link href={"/sell"}>Sell</Link>
         </NavItem>
         <NavItem>Rent</NavItem>
       </div>
@@ -31,12 +40,20 @@ function Header() {
       </div>
       <div className="flex justify-start items-center gap-7">
         <NavItem isBold={true}>
-          <Link href={"/"}>Home</Link>{" "}
+          <Link href={"/"}>Home</Link>
         </NavItem>
         <NavItem>Help</NavItem>
-        <Link href="/sign_in">
-        <NavItem>Sign In</NavItem>
-      </Link>
+        {currentUser ? (
+          <Link href="/profile">
+            <NavItem>
+              <User className="text-white w-6 h-6" />
+            </NavItem>
+          </Link>
+        ) : (
+          <Link href="/sign_in">
+            <NavItem>Sign In</NavItem>
+          </Link>
+        )}
       </div>
     </div>
   );

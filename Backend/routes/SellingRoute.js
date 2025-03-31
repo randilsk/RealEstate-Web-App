7021b3818a62523ab1bc9715f0ccc90852251760
randilsk <randilsulakkana@gmail.com>
2025-03-31 20:02:21 +0530

    updated

--- Backend/routes/SellingRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createPropertyListing, upload } from '../controllers/sellerController.js';
-
-const router = express.Router();
-
-// Route to create a new property listing
-router.post('/create-listing', upload.array('photos', 10), createPropertyListing);
-
-export default router;

7bfbd9da86f8e6be30a89bc48e0bc5b10a3a58d3
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-04-06 15:24:42 +0530

    add signout

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,14 +1,12 @@
 import express from 'express';
-import { signup,signin, google } from '../controllers/auth.controlller.js';
+import { signup, signin, google, signout, deleteUser } from '../controllers/auth.controlller.js';
 
 const router = express.Router();  
 
-
-
-router.post('/signup',signup);
-router.post('/signin',signin);
-router.post('/google',google);
-
-
+router.post('/signup', signup);
+router.post('/signin', signin);
+router.post('/google', google);
+router.get('/signout', signout);
+router.delete('/delete/:userId', deleteUser);
 
 export default router;

95096d941b6a873845914817c5f4dcaa9e6afc13
randilsk <randilsulakkana@gmail.com>
2025-04-06 15:29:21 +0530

    buy page going on

---
+++ frontend/src/components/BuyPageContent/CardSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card"; // Corrected import path
+
+function CardSection() {
+  return (
+    <div className="w-5/12 h-screen mt-10">
+      <Card />
+    </div>
+  );
+}
+
+export default CardSection;

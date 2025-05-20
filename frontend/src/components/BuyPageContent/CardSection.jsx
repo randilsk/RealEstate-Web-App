bbe237c7e02999dbb27380e7060b473314a50e78
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-05-20 20:18:02 +0530

    dynamic the card section

--- frontend/src/components/BuyPageContent/CardSection.jsx
+++ frontend/src/components/BuyPageContent/CardSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Card from "./Card"; // Corrected import path
 
+
+
 function CardSection() {
   return (
     <div className="w-1/2 h-screen mt-5 px-5">
-      <h2 className="font-semibold py-2">Real states and homes for sale</h2>
+      <h2 className="font-semibold py-2 text-3xl pl-5">Recent Properties</h2>
       <div className=" flex">
         <Card />
       </div>

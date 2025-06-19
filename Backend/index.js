26824ac3420dbca874a98ed1bc609990e184940a
randilsk <randilsulakkana@gmail.com>
2025-06-20 01:38:08 +0530

    for merging, no errors

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import reviewRouts from "./routes/reviewRoutes.js"
 import rentRoutes from "./routes/Rentroutes.js"
+import AdminRoute from "./routes/admin.route.js";
 
 dotenv.config();
 
@@ -43,6 +44,7 @@ app.use("/api/user", UserRoute);
 app.use("/api/auth", AuthRouter);
 app.use("/api/listing", ListingRoute);
 app.use("/api/reviews", reviewRouts);
+app.use("/api/admin", AdminRoute);
 app.use("/api/Rentroutes",rentRoutes);
 
 //global error handling middleware

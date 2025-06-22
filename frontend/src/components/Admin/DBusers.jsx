220c6d2537576e35f277071a1cb17c2835a179b5
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-06-23 01:38:35 +0530

    did some changes

--- frontend/src/components/Admin/DBusers.jsx
+++ frontend/src/components/Admin/DBusers.jsx
@@ -314,12 +314,9 @@ function DBusers() {
     return (
         <div className="flex-2 bg-gray-100 min-h-screen">
             {/* Navbar */}
-            <div className="bg-[#3B50DF] shadow-md p-4 flex justify-between items-center text-white">
-                <input type="text" placeholder="Enter an address, city, district, province" className="p-2 border rounded-md w-1/3 text-black" />
-                <div className="flex gap-4 text-xl">
-                    <FaBell className="cursor-pointer hover:text-blue-200 transition-colors" />
-                    <FaUserCircle className="cursor-pointer hover:text-blue-200 transition-colors" />
-                </div>
+            <div className="  p-4 flex justify-center items-center text-white">
+                <input type="text" placeholder="Enter an address, city, district, province" className="p-2 border rounded-md w-1/3 text-black border-[#3b50df] rounded-[50px]" />
+                
             </div>
 
             {/* Dashboard Stats Section */}

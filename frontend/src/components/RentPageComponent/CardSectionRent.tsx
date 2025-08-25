08050abf007c80963f8545d32f4e5efd42c73018
Nuwan Tharindu <nuwan.tharidu0806@gmail.com>
2025-08-25 15:24:14 +0530

    feat: implement CardSectionRent component with dynamic import of BuyCardSection

--- frontend/src/components/RentPageComponent/CardSectionRent.tsx
+++ frontend/src/components/RentPageComponent/CardSectionRent.tsx
@@ -1,9 +1,20 @@
+"use client";
 import React from 'react';
+import dynamic from 'next/dynamic';
 
-function CardSectionRent() {
-    return (
-        <div>ComponentName</div>
-    );
+// Dynamically import the buy CardSection to keep parity and avoid SSR issues
+const BuyCardSection = dynamic(() => import('@/components/BuyPageContent/CardSection'));
+
+type Listing = {
+  _id: string;
+};
+
+interface CardSectionRentProps {
+  listings: Listing[];
+}
+
+function CardSectionRent({ listings }: CardSectionRentProps) {
+  return <BuyCardSection listings={listings} />;
 }
 
 export default CardSectionRent;

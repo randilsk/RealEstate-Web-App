453373a4f18bc33dedb04148400274790febcf6d
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-08-27 08:56:59 +0530

    delete account part is working now

--- frontend/src/app/sign-up/page.jsx
+++ frontend/src/app/sign-up/page.jsx
@@ -41,6 +41,7 @@ export default function SignUp() {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify(formData),
       });
       const data = await res.json();
